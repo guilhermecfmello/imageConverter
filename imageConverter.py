84fb9a91d7feeee263ca3374d9d5b58611e96542
Guilherme Mello <guilhermecfmello@gmail.com>
2020-07-07 11:53:37 -0300

    rgb converter and normalizer implemented but not tested

--- imageConverter.py
+++ imageConverter.py
@@ -3,6 +3,8 @@ import matplotlib.pyplot as plt
 # from PIL import Image, ImageDraw
 import numpy as np
 import scipy.misc as smp
+import math as m
+
 
 RGB_MAX = 255
 
@@ -18,7 +20,19 @@ class ImageConverter:
 		print("Starting convertion of image: " + self.imgName + " to format HSI...")
 		print("At the end, you'll be able to see the normalized image.")
 
-		exit("hsi")
+		img = imread(self.imgName)
+
+		x = len(img)
+		y = len(img[0])
+		data = np.zeros((x,y,3), dtype=np.uint8 )
+		originalData = data
+		for i in range(len(img)):
+			for j in range(len(img[i])):
+				pixel = self.__rgbToHsiPix(img[i][j][0], img[i][j][1], img[i][j][2])
+				newPixel = self.__hsiRgbNormalizer(pixel)
+				data[i][j] = newPixel
+		imgPlot = plt.imshow(data)
+		plt.show()
 
 	def rgbToCmyk(self):
 		print("Starting convertion of image: " + self.imgName + " to format CMYK...")
@@ -35,12 +49,8 @@ class ImageConverter:
 				pixel = self.__rgbToCmykPix(img[i][j][0], img[i][j][1], img[i][j][2])
 				newPixel = self.__cmykRgbNormalizer(pixel)
 				data[i][j] = newPixel
-				
-		# print(img[0][0])
 		imgPlot = plt.imshow(data)
-		
 		plt.show()
-		# plt.show()
 
 	def __rgbToCmykPix(self, r, g, b):
 		if(r,g,b) == (0,0,0):
@@ -60,6 +70,45 @@ class ImageConverter:
 
 		return c, m, y, k
 	
+	def __rgbToHsiPix(self, r, g, b):
+		
+		theta = (((r-g)+(r-b))/2)/(((r-g)**2+((r-b)*(g-b)))/2)
+		theta = m.acos(theta)
+		h = 0
+		if b <= g:
+			h = theta
+		else:
+			h = 360 - theta
+		
+		s = 1-((3/(r+g+b))*min(r,g,b))
+		i = (r+g+b)/3
+
+		return h,s,i
+
+	def __hsiRgbNormalizer(self, hsi):
+		h,s,i = hsi[0],hsi[1],hsi[2]
+		r,g,b = 0
+		if h >= 0 and h < 120:
+			b = i*(1-s])
+			r = i*(1 + (s*m.cos(h)/m.cos(60-h)))
+			g = 3*i-(r+b)
+		elif h >= 120 and h < 240:
+			h -= 120
+			r = i*(1-s)
+			g = i*(1+(s*m.cos(h)/m.cos(60-h)))
+			b = 3*i-(r+g)
+		elif h >= 240 and h < 360:
+			h -= 240
+			g = i*(1-s)
+			b = i*(1+(s*m.cos(h)/m.cos(60-h)))
+			r = 3*i-(g+b)
+		else:
+			r,g,b = 255,255,255
+
+		return [r,g,b]
+
+
+
 	def __cmykRgbNormalizer(self, cmyk):
 		k = cmyk[3]
 		r = (cmyk[0] + k) * 255
